096386742708aeb0f63e0a9004c9abb0e3fcf88c
Ayeza Bashir <ayezabashir46@gmail.com>
2026-03-26 19:29:07 +0500

    hero section

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 const logoTl = gsap.timeline();
 logoTl.from(".letter", {
   stagger: -0.09,
-  y: 100,
+  y: -100,
   opacity: 0,
+  scrollTrigger: {},
 });
 
 function cursorEffect() {
-  const page1Content = document.getElementById("page1-content");
+  const page1Content = document.getElementById("page2");
   const cursor = document.getElementById("cursor");
 
   page1Content.addEventListener("mousemove", (details) => {
@@ -29,4 +30,69 @@ function cursorEffect() {
   });
 }
 
+function navigationEffect() {
+  const listItems = document.querySelectorAll(".item");
+  listItems.forEach((item) => {
+    let underline = item.querySelector(".underline");
+    item.addEventListener("mouseover", () => {
+      underline.style.display = "inline-block";
+      gsap.fromTo(
+        underline,
+        {
+          width: "0%",
+          left: "0%",
+        },
+        {
+          width: "100%",
+          duration: 0.5,
+        },
+      );
+    });
+    item.addEventListener("mouseleave", () => {
+      gsap.fromTo(
+        underline,
+        {
+          width: "100%",
+          left: "0%",
+        },
+        {
+          width: "0%",
+          left: "100%",
+          duration: 0.5,
+          immediateRender: false,
+        },
+      );
+    });
+
+    item.addEventListener("click", () => {
+      const isActive = item.classList.contains("active");
+      listItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      if (!isActive) {
+        item.classList.add("active");
+      }
+    });
+  });
+}
+
+function textEffect(text) {
+  text.addEventListener("mouseenter", () => {
+    gsap.to(text, {
+      duration: 0.3,
+      x:0,
+    });
+  });
+  text.addEventListener("mouseleave", () => {
+    gsap.to(text, {
+      duration: 0.3,
+      x:-18,
+    });
+  });
+}
+
+let talkText = document.querySelector(".talk p");
+
 cursorEffect();
+navigationEffect();
+textEffect(talkText);
